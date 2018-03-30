d148e5108b99e72ffa8d684ccfb09f6cafaa0bb8
David Bowen <dsbmusic328@gmail.com>
2018-03-30 19:54:35 -0400

    updated database connection string for heroku db

--- databases/db1.js
+++ databases/db1.js
@@ -3,10 +3,10 @@ const knex = require('knex')
 const db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
-        user: 'davidbowen',
-        password: '',
-        database: 'smart-brain'
+        host: 'ec2-23-21-121-220.compute-1.amazonaws.com',
+        user: 'qbtdsljwzjyoly',
+        password: 'a8df1259ee46b49552e1125536aad758e42c0bee506ed825096c6a8de4c61168',
+        database: 'dqehskl5raahd'
     }
 });
 
